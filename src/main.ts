560369d6c82c2b7e0af94c60d4aef7553346b477
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2022-03-16 09:26:23 +0100

    axios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import axios from 'axios';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+
+  console.log('starting!!!');
+  try {
+    const result = await axios.post(
+      'http://aloha.olimpia.hu:4000/api',
+      'tags',
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    console.log(result.data);
+  } catch (error) {
+    console.log('error!!!!!!');
+    console.log(error);
+  }
 }
 bootstrap();
