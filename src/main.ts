723a5ca48a53171dbfe32f800291c28a2b452144
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2022-03-16 10:58:06 +0100

    getLowResImage

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
 async function bootstrap() {
-  await getSpecialTags();
+  await getLowResImage();
 }
 bootstrap();
+
+interface IAllTagsResponse {
+  tags: string[];
+}
 
 async function getTags() {
   console.log('Listing all tags...');
   try {
-    const result = await axios.post(
+    const { data } = await axios.post<IAllTagsResponse>(
       'http://aloha.olimpia.hu:4000/api',
       'tags',
       {
@@ -18,17 +22,30 @@ async function getTags() {
       },
     );
 
-    console.log(result.data);
+    console.log(data.tags);
   } catch (error) {
     console.log('error!!!!!!');
     console.log(error);
   }
 }
 
+interface ISpecialTagsResponse {
+  results: string[];
+}
+
+// {
+//   tags: [
+//     'Diaz',
+//     '1956',
+//     '2014',
+//     '2014.08.01',
+//     '2014.08.03',
+//   ]
+// }
 async function getSpecialTags() {
   console.log('Listing special tags...');
   try {
-    const result = await axios.post(
+    const { data } = await axios.post<ISpecialTagsResponse>(
       'http://aloha.olimpia.hu:4000/api',
       'lookup=["2014","kenu"]',
       {
@@ -38,7 +55,36 @@ async function getSpecialTags() {
       },
     );
 
-    console.log(result.data);
+    console.log(data.results);
+  } catch (error) {
+    console.log('error!!!!!!');
+    console.log(error);
+  }
+}
+
+// {
+//   results: [
+//     '20140922_ute_kajak_kenu_avatas/20140922_ute_kajak_kenu_nevado_007.jpg',
+//     '20140922_ute_kajak_kenu_avatas/20140922_ute_kajak_kenu_nevado_008.jpg',
+//     '20140922_ute_kajak_kenu_avatas/20140922_ute_kajak_kenu_nevado_009.jpg',
+//     '20140922_ute_kajak_kenu_avatas/20140922_ute_kajak_kenu_nevado_015.jpg'
+//   ]
+// }
+async function getLowResImage() {
+  console.log('Listing low res image...');
+  try {
+    const { data, headers } = await axios.post<ISpecialTagsResponse>(
+      'http://aloha.olimpia.hu:4000/api',
+      'preview=20140922_ute_kajak_kenu_avatas/20140922_ute_kajak_kenu_nevado_015.jpg',
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    console.log({ data });
+    console.log({ headers });
   } catch (error) {
     console.log('error!!!!!!');
     console.log(error);
